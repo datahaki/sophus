c47a8f5e86987164d2d1dc57ca262a07da39e41b
datahaki <jan.hakenberg@gmail.com>
2021-09-24 22:50:12 +0200

    rn dbscan

--- src/main/java/ch/alpine/sophus/lie/rn/RnDbscan.java
+++ src/main/java/ch/alpine/sophus/lie/rn/RnDbscan.java
@@ -4,6 +4,7 @@ package ch.alpine.sophus.lie.rn;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -16,47 +17,38 @@ import ch.alpine.tensor.opt.nd.NdClusterRadius;
 import ch.alpine.tensor.opt.nd.NdMap;
 import ch.alpine.tensor.opt.nd.NdMatch;
 import ch.alpine.tensor.opt.nd.NdTreeMap;
-import ch.alpine.tensor.sca.Sign;
 
 /** Density-based spatial clustering of applications with noise
  * 
  * Reference: Wikipedia */
-public class RnDbscan {
-  private static final int NOISE = -1;
-  // ---
-  private final Scalar eps;
-  private final int minPts;
+public enum RnDbscan {
+  ;
+  public static final int NOISE = -1;
 
-  public RnDbscan(Scalar eps, int minPts) {
-    this.eps = Sign.requirePositiveOrZero(eps);
-    this.minPts = Integers.requirePositive(minPts);
-  }
-
-  public Integer[] cluster(Tensor points, Function<Tensor, NdCenterInterface> function) {
+  /** @param points
+   * @param function
+   * @param radius non-negative
+   * @param minPts positive
+   * @return array of labels for each point, or -1 to indicate that respective point is noise */
+  public static Integer[] of(Tensor points, Function<Tensor, NdCenterInterface> function, Scalar radius, int minPts) {
+    Integers.requirePositive(minPts);
     NdMap<Integer> ndMap = NdTreeMap.of(MinMax.ndBox(points));
-    {
-      int index = -1;
-      for (Tensor point : points)
-        ndMap.add(point, ++index);
-    }
+    AtomicInteger atomicInteger = new AtomicInteger();
+    points.stream().forEach(point -> ndMap.add(point, atomicInteger.getAndIncrement()));
     // ---
     Integer[] labels = new Integer[points.length()];
     int index = 0;
     int cluster = -1;
     for (Tensor p : points) {
       if (Objects.isNull(labels[index])) { // else "Previously processed in inner loop"
-        Collection<NdMatch<Integer>> c_p = //
-            NdClusterRadius.of(ndMap, function.apply(p), eps); // "Find neighbors"
-        if (c_p.size() < minPts) { // "Density check"
+        Collection<NdMatch<Integer>> c_p = NdClusterRadius.of(ndMap, function.apply(p), radius);
+        if (c_p.size() < minPts) // "Density check"
           labels[index] = NOISE; // "Label as Noise"
-        } else {
+        else {
           ++cluster; // "next cluster label"
           labels[index] = cluster; // "Label initial point"
-          // ---
           Set<Integer> set = c_p.stream().map(NdMatch::value).collect(Collectors.toSet());
-          boolean removed = set.remove(index);
-          if (!removed)
-            System.err.println("not removed?");
+          set.remove(index);
           while (!set.isEmpty()) {
             int index_q = set.iterator().next(); // "Process every seed point Q"
             set.remove(index_q);
@@ -66,10 +58,9 @@ public class RnDbscan {
             if (Objects.isNull(labels[index_q])) { // else "Previously processed (e.g., border point)"
               labels[index_q] = cluster; // "Label neighbor"
               Tensor q = points.get(index_q);
-              Collection<NdMatch<Integer>> c_q = //
-                  NdClusterRadius.of(ndMap, function.apply(q), eps); // "Find neighbors"
+              Collection<NdMatch<Integer>> c_q = NdClusterRadius.of(ndMap, function.apply(q), radius);
               if (minPts <= c_q.size()) // "Density check (if Q is a core point)"
-                c_p.stream().map(NdMatch::value).forEach(set::add); // "Add new neighbors to seed set"
+                c_q.stream().map(NdMatch::value).forEach(set::add); // "Add new neighbors to seed set"
             }
           }
         }
--- src/test/java/ch/alpine/sophus/lie/rn/RnDbscanTest.java
+++ src/test/java/ch/alpine/sophus/lie/rn/RnDbscanTest.java
@@ -4,20 +4,46 @@ package ch.alpine.sophus.lie.rn;
 import ch.alpine.sophus.math.sample.BoxRandomSample;
 import ch.alpine.sophus.math.sample.RandomSample;
 import ch.alpine.sophus.math.sample.RandomSampleInterface;
+import ch.alpine.sophus.usr.AssertFail;
 import ch.alpine.tensor.RealScalar;
 import ch.alpine.tensor.Tensor;
 import ch.alpine.tensor.Tensors;
+import ch.alpine.tensor.alg.Array;
+import ch.alpine.tensor.alg.ConstantArray;
+import ch.alpine.tensor.alg.Range;
 import ch.alpine.tensor.opt.nd.NdBox;
 import ch.alpine.tensor.opt.nd.NdCenterBase;
 import junit.framework.TestCase;
 
 public class RnDbscanTest extends TestCase {
   public void testSimple() {
-    
-    RnDbscan rnDbscan = new RnDbscan(RealScalar.of(0.1), 3);
     RandomSampleInterface randomSampleInterface = BoxRandomSample.of(NdBox.of(Tensors.vector(0, 0), Tensors.vector(1, 1)));
     Tensor points = RandomSample.of(randomSampleInterface, 40);
-    Integer[] integers = rnDbscan.cluster(points, NdCenterBase::of2Norm);
-    assertEquals(integers.length, 40);
+    Integer[] integers = RnDbscan.of(points, NdCenterBase::of2Norm, RealScalar.of(0.1), 3);
+    assertEquals(integers.length, points.length());
+  }
+
+  public void testUniform() {
+    Tensor points = Range.of(0, 8).map(Tensors::of);
+    Integer[] integers = RnDbscan.of(points, NdCenterBase::of2Norm, RealScalar.of(1.1), 3);
+    assertEquals(Tensors.vector(integers), Array.zeros(integers.length));
+  }
+
+  public void testInsufficientRadius() {
+    Tensor points = Range.of(0, 8).map(Tensors::of);
+    Integer[] integers = RnDbscan.of(points, NdCenterBase::of2Norm, RealScalar.of(0.1), 2);
+    assertEquals(Tensors.vector(integers), ConstantArray.of(RealScalar.of(-1), integers.length));
+  }
+
+  public void testInsufficientPoints() {
+    Tensor points = Range.of(0, 8).map(Tensors::of);
+    Integer[] integers = RnDbscan.of(points, NdCenterBase::of2Norm, RealScalar.of(1.1), 4);
+    assertEquals(Tensors.vector(integers), ConstantArray.of(RealScalar.of(-1), integers.length));
+  }
+
+  public void testFail() {
+    Tensor points = Range.of(0, 8).map(Tensors::of);
+    AssertFail.of(() -> RnDbscan.of(points, NdCenterBase::of2Norm, RealScalar.of(-1.1), 3));
+    AssertFail.of(() -> RnDbscan.of(points, NdCenterBase::of2Norm, RealScalar.of(+1.1), 0));
   }
 }
