598843fa3a68f0cc05088c17645181f5aa07d3dc
datahaki <jan.hakenberg@gmail.com>
2022-07-30 12:35:10 +0200

    pom

--- src/main/java/ch/alpine/sophus/hs/r3/qh3/ConvexHull3D.java
+++ src/main/java/ch/alpine/sophus/hs/r3/qh3/ConvexHull3D.java
@@ -9,12 +9,12 @@ import ch.alpine.tensor.io.Primitives;
 /** uses QuickHull3D by John E. Lloyd, Fall 2004 */
 public enum ConvexHull3D {
   ;
-  /** @param tensor
+  /** @param tensor of size n x 3
    * @return */
   public static int[][] of(Tensor tensor) {
     tensor.stream().forEach(vector -> VectorQ.requireLength(vector, 3));
-    QuickHull3D quickHull3D = new QuickHull3D();
-    quickHull3D.build(Primitives.toDoubleArray(tensor));
-    return quickHull3D.getFaces(QuickHull3D.POINT_RELATIVE);
+    QuickHull3D quickHull3D = new QuickHull3D(Primitives.toDoubleArray(tensor));
+    quickHull3D.buildHull();
+    return quickHull3D.getFaces();
   }
 }
