2cafc142429244e96e73e6e6ce7527bebc65d23b
datahaki <jan.hakenberg@gmail.com>
2022-08-05 14:02:16 +0200

    simplex d

--- src/test/java/ch/alpine/sophus/hs/r3/PlatonicSolidTest.java
+++ src/test/java/ch/alpine/sophus/hs/r3/PlatonicSolidTest.java
@@ -2,11 +2,18 @@
 package ch.alpine.sophus.hs.r3;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.Set;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
+
+import ch.alpine.sophus.math.SimplexD;
+import ch.alpine.tensor.Tensor;
+import ch.alpine.tensor.mat.Tolerance;
+import ch.alpine.tensor.red.Mean;
 
 class PlatonicSolidTest {
   @ParameterizedTest
@@ -20,5 +27,13 @@ class PlatonicSolidTest {
   @ParameterizedTest
   @EnumSource
   void testCenter(PlatonicSolid platonicSolid) {
+    Tolerance.CHOP.requireAllZero(Mean.of(platonicSolid.vertices()));
+  }
+
+  @ParameterizedTest
+  @EnumSource
+  void testBorder(PlatonicSolid platonicSolid) {
+    Set<Tensor> border = SimplexD.of(platonicSolid.faces());
+    assertTrue(border.isEmpty());
   }
 }
