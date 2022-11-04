1bbaa2355f7bf552f07f21e880ab30b67d213390
datahaki <jan.hakenberg@gmail.com>
2022-11-04 23:06:19 +0100

    plausible rational

--- src/main/java/ch/alpine/sophus/math/PlausibleRational.java
+++ src/main/java/ch/alpine/sophus/math/PlausibleRational.java
@@ -1,17 +1,14 @@
 // code by jph
 package ch.alpine.sophus.math;
 
-import ch.alpine.tensor.ComplexScalar;
+import ch.alpine.tensor.MultiplexScalar;
 import ch.alpine.tensor.RealScalar;
 import ch.alpine.tensor.Scalar;
-import ch.alpine.tensor.Scalars;
 import ch.alpine.tensor.api.ScalarUnaryOperator;
 import ch.alpine.tensor.chq.ExactScalarQ;
 import ch.alpine.tensor.mat.Tolerance;
 import ch.alpine.tensor.num.Rationalize;
-import ch.alpine.tensor.qty.Quantity;
 
-// EXPERIMENTAL
 public enum PlausibleRational implements ScalarUnaryOperator {
   FUNCTION;
 
@@ -19,22 +16,16 @@ public enum PlausibleRational implements ScalarUnaryOperator {
       Rationalize.withDenominatorLessEquals(10_000);
 
   @Override
-  public Scalar apply(Scalar t) {
-    if (ExactScalarQ.of(t))
-      return t;
-    if (t instanceof Quantity q)
-      return Quantity.of(q.value(), q.unit());
-    if (t instanceof ComplexScalar c)
-      return ComplexScalar.of( //
-          apply(c.real()), //
-          apply(c.imag()));
-    if (t instanceof RealScalar) {
-      if (Scalars.isZero(t))
-        return RealScalar.ZERO;
-      Scalar approx = RATIONALIZE.apply(t);
-      if (Tolerance.CHOP.isClose(approx, t))
+  public Scalar apply(Scalar scalar) {
+    if (scalar instanceof MultiplexScalar multiplexScalar)
+      return multiplexScalar.eachMap(FUNCTION);
+    if (ExactScalarQ.of(scalar))
+      return scalar;
+    if (scalar instanceof RealScalar) {
+      Scalar approx = RATIONALIZE.apply(scalar);
+      if (Tolerance.CHOP.isClose(approx, scalar))
         return approx;
     }
-    return t;
+    return scalar;
   }
 }
--- src/test/java/ch/alpine/sophus/math/PlausibleRationalTest.java
+++ src/test/java/ch/alpine/sophus/math/PlausibleRationalTest.java
@@ -1,14 +1,34 @@
 // code by jph
 package ch.alpine.sophus.math;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+
+import ch.alpine.tensor.ComplexScalar;
+import ch.alpine.tensor.RationalScalar;
+import ch.alpine.tensor.RealScalar;
+import ch.alpine.tensor.Scalar;
+import ch.alpine.tensor.Scalars;
+import ch.alpine.tensor.chq.ExactScalarQ;
+import ch.alpine.tensor.sca.Re;
 
 class PlausibleRationalTest {
   @Test
-  void testSimple() {
-    // Scalar v = PlausibleRational.FUNCTION.apply(RealScalar.of(-0.8660254037844389));
-    // System.out.println(v);
-    //
-    // -489061/564719
+  void testComplex() {
+    Scalar scalar = PlausibleRational.FUNCTION.apply(ComplexScalar.of(0.5, 0.2344523));
+    assertEquals(RationalScalar.HALF.toString(), Re.FUNCTION.apply(scalar).toString());
+  }
+
+  @Test
+  void testQuantity() {
+    Scalar scalar = PlausibleRational.FUNCTION.apply(Scalars.fromString("0.333333333333333333333333333[m]"));
+    assertEquals(scalar.toString(), "1/3[m]");
+  }
+
+  @Test
+  void testPlausibleRational() {
+    Scalar scalar = PlausibleRational.FUNCTION.apply(RealScalar.of(0.0));
+    ExactScalarQ.require(scalar);
   }
 }
